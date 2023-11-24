0d5c9ecf05ae10d05e1b4f10ce0ab18971c02858
Leandro Anriquez <Leandro@MacBook-Pro-de-Leandro.local>
2023-11-24 14:40:15 -0300

    Inicio de proyecto

---
+++ app/src/main/java/com/example/ladespedida/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ladespedida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends sinBarraSuperior{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
