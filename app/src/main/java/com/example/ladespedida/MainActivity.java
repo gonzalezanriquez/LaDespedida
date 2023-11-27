ed2b42c4a4f3b840505c0abfc9e8e9e0abd7a461
Leandro <gonzalezanriquez@gmail.com>
2023-11-27 18:00:53 -0300

    conexion

--- app/src/main/java/com/example/ladespedida/MainActivity.java
+++ app/src/main/java/com/example/ladespedida/MainActivity.java
@@ -1,74 +1,143 @@
 package com.example.ladespedida;
 
-import android.Manifest;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothManager;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-public class MainActivity extends sinBarraSuperior {
-    private BluetoothGatt bluetoothGatt;  // Almacena la instancia de BluetoothGatt para la conexión BLE
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager;
+    private ListView listView;
+    private Button scanButton;
+    private List<ScanResult> results;
+
+    private static final int PERMISSION_REQUEST_CODE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnConnect = findViewById(R.id.btnConnect);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
-        btnConnect.setOnClickListener(new View.OnClickListener() {
+        listView = findViewById(R.id.listView);
+        scanButton = findViewById(R.id.btnScan);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                connectToESP32();  // Método para conectar con el ESP32
+                checkAndRequestPermissions();
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Acciones cuando se hace clic en un elemento de la lista
+                String ssid = results.get(position).SSID;
+                Toast.makeText(getApplicationContext(), "Conectando a: " + ssid, Toast.LENGTH_SHORT).show();
+                // Aquí puedes implementar la lógica para conectarte a la red WiFi seleccionada
             }
         });
     }
 
-    private void connectToESP32() {
-        // Verifica si el dispositivo admite BLE
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            // El dispositivo no admite BLE
-            // Puedes manejar esto según tus necesidades
-            return;
+
+    private void checkAndRequestPermissions() {
+        String[] permissions = {
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.CHANGE_WIFI_STATE
+        };
+
+        boolean allPermissionsGranted = true;
+
+        for (String permission : permissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                allPermissionsGranted = false;
+                break;
+            }
         }
 
-        // Solicita permisos de Bluetooth
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1);
-            return;
+        if (!allPermissionsGranted) {
+            ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE);
+        } else {
+            // Si todos los permisos ya están concedidos, realiza la operación que requiere los permisos
+            scanWifi();
         }
-
-        // Inicializa el adaptador Bluetooth
-        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
-
-        // Verifica si el Bluetooth está habilitado
-        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, 2);
-            return;
-        }
-
-        // Escanea y encuentra el dispositivo ESP32 por su nombre o dirección MAC
-        BluetoothDevice esp32Device = bluetoothAdapter.getRemoteDevice("Nombre del dispositivo o dirección MAC");
-
-        // Conecta con el dispositivo
-        bluetoothGatt = esp32Device.connectGatt(this, false, new BluetoothGattCallback() {
-            // Implementa los métodos callback para manejar la conexión y la comunicación
-        });
     }
+
+
+    private void scanWifi() {
+        wifiManager.startScan();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        results = wifiManager.getScanResults();
+        displayWifiList();
+    }
+
+    private void displayWifiList() {
+        String[] wifiArray = new String[results.size()];
+        for (int i = 0; i < results.size(); i++) {
+            wifiArray[i] = results.get(i).SSID;
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, wifiArray);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            boolean allPermissionsGranted = true;
+
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    allPermissionsGranted = false;
+                    break;
+                }
+            }
+
+            if (allPermissionsGranted) {
+                // Todos los permisos han sido concedidos, realiza la operación que requiere los permisos
+                scanWifi();
+            } else {
+                // Al menos uno de los permisos fue denegado, muestra un mensaje o toma alguna acción
+                Toast.makeText(this, "Permiso(s) denegado(s)", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
+
