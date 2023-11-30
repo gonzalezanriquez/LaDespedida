21fe9f852d5bf6f792a16ef6e026fd805eaa5235
Leandro <Leandro@DESKTOP-01GF9J5>
2023-11-30 00:27:55 -0300

    last

--- app/src/main/java/com/example/ladespedida/MainActivity.java
+++ app/src/main/java/com/example/ladespedida/MainActivity.java
@@ -1,143 +1,174 @@
 package com.example.ladespedida;
 
-import android.content.Context;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.pm.PackageManager;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
+import android.os.Handler;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WifiManager wifiManager;
-    private ListView listView;
-    private Button scanButton;
-    private List<ScanResult> results;
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothSocket socket;
+    private InputStream inputStream;
+    private TextView sensorValueTextView, timerTextView;
 
-    private static final int PERMISSION_REQUEST_CODE = 123;
+    private Handler handler = new Handler();
+    private StringBuilder data = new StringBuilder();
+    private boolean timerRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        sensorValueTextView = findViewById(R.id.sensorValueTextView);
+        timerTextView = findViewById(R.id.timerTextView);
 
-        listView = findViewById(R.id.listView);
-        scanButton = findViewById(R.id.btnScan);
-
-        scanButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAndRequestPermissions();
-            }
-        });
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Acciones cuando se hace clic en un elemento de la lista
-                String ssid = results.get(position).SSID;
-                Toast.makeText(getApplicationContext(), "Conectando a: " + ssid, Toast.LENGTH_SHORT).show();
-                // Aquí puedes implementar la lógica para conectarte a la red WiFi seleccionada
-            }
-        });
-    }
-
-
-    private void checkAndRequestPermissions() {
-        String[] permissions = {
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.CHANGE_WIFI_STATE
-        };
-
-        boolean allPermissionsGranted = true;
-
-        for (String permission : permissions) {
-            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                allPermissionsGranted = false;
-                break;
-            }
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // El dispositivo no soporta Bluetooth
+            return;
         }
 
-        if (!allPermissionsGranted) {
-            ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE);
+        // Dirección MAC del módulo Bluetooth
+        String address = "00:00:00:00:00:00"; // Reemplazar con la dirección del ESP32/ESP8266
+
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
+
+        try {
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            socket = device.createRfcommSocketToServiceRecord(uuid);
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            socket.connect();
+            inputStream = socket.getInputStream();
+
+            listenToData();
+            startTimer(); // Comienza el temporizador al conectarse exitosamente
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void listenToData() {
+        final byte delimiter = 10; // El carácter '\n' es el delimitador
+
+        Thread workerThread = new Thread(new Runnable() {
+            byte[] readBuffer = new byte[1024];
+            int readBufferPosition = 0;
+
+            public void run() {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        int bytesAvailable = inputStream.available();
+                        if (bytesAvailable > 0) {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            inputStream.read(packetBytes);
+
+                            for (int i = 0; i < bytesAvailable; i++) {
+                                byte b = packetBytes[i];
+                                if (b == delimiter) {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String receivedData = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable() {
+                                        public void run() {
+                                            processData(receivedData.trim());
+                                        }
+                                    });
+                                } else {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    } catch (IOException ex) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+    }
+
+    private void processData(String receivedData) {
+        if (timerRunning) {
+            data.append(receivedData).append("\n");
         } else {
-            // Si todos los permisos ya están concedidos, realiza la operación que requiere los permisos
-            scanWifi();
+            // Mostrar el último valor después de que el temporizador haya expirado
+            sensorValueTextView.setText("Último valor: " + receivedData);
         }
     }
 
+    private void startTimer() {
+        timerRunning = true;
 
-    private void scanWifi() {
-        wifiManager.startScan();
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        results = wifiManager.getScanResults();
-        displayWifiList();
-    }
+        new CountDownTimer(10000, 1000) { // Temporizador de 10 segundos (10000 ms) con actualización cada segundo (1000 ms)
+            public void onTick(long millisUntilFinished) {
+                timerTextView.setText("Tiempo restante: " + millisUntilFinished / 1000 + " segundos");
+            }
 
-    private void displayWifiList() {
-        String[] wifiArray = new String[results.size()];
-        for (int i = 0; i < results.size(); i++) {
-            wifiArray[i] = results.get(i).SSID;
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, wifiArray);
-        listView.setAdapter(adapter);
+            public void onFinish() {
+                timerRunning = false;
+                // Mostrar el último valor después de que el temporizador haya expirado
+                if (data.length() > 0) {
+                    String lastValue = data.toString().trim().split("\n")[data.toString().trim().split("\n").length - 1];
+                    sensorValueTextView.setText("Último valor después del tiempo: " + lastValue);
+                }
+                timerTextView.setText("Tiempo terminado");
+            }
+        }.start();
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            boolean allPermissionsGranted = true;
-
-            for (int result : grantResults) {
-                if (result != PackageManager.PERMISSION_GRANTED) {
-                    allPermissionsGranted = false;
-                    break;
-                }
-            }
-
-            if (allPermissionsGranted) {
-                // Todos los permisos han sido concedidos, realiza la operación que requiere los permisos
-                scanWifi();
-            } else {
-                // Al menos uno de los permisos fue denegado, muestra un mensaje o toma alguna acción
-                Toast.makeText(this, "Permiso(s) denegado(s)", Toast.LENGTH_SHORT).show();
-            }
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            socket.close(); // Cierra la conexión al destruir la actividad
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-
 }
-
